22da6ebf148d28d79b84179ef354c9043cde9a66
jcbritobr <jcbritobr@gmail.com>
2021-06-29 11:34:37 -0300

    Fix text format.

--- src/fl_text_display.rs
+++ src/fl_text_display.rs
@@ -5,22 +5,32 @@ use fltk::{
     window::Window,
 };
 
+const WIDGET_PADDING: i32 = 20;
+const WIDGET_WIDTH: i32 = 640;
+const WIDGET_HEIGHT: i32 = 480;
+
 fn main() {
     let app = App::default().with_scheme(Scheme::Gtk);
-    let mut win = Window::default().with_size(640, 480);
+    let mut win = Window::default().with_size(WIDGET_WIDTH, WIDGET_HEIGHT);
 
     let mut text_buffer = TextBuffer::default();
     text_buffer.set_text(
-        "line 0\nline 1\nline 2
-line 3\nline 4\nline 5
-line 6\nline 7\nline 8
-line 9\nline 10\nline 11
-line 12\nline 13\nline 14
-line 15\nline 16\nline 17
-line 18\nline 19\nline 10
-line 21\nline 22\nline 23",
+        "line 0\nline 1\nline 2\n\
+            line 3\nline 4\nline 5\n\
+            line 6\nline 7\nline 8\n\
+            line 9\nline 10\nline 11\n\
+            line 12\nline 13\nline 14\n\
+            line 15\nline 16\nline 17\n\
+            line 18\nline 19\nline 10\n\
+            line 21\nline 22\nline 23",
     );
-    let mut disp = TextDisplay::new(20, 20, 640 - 40, 480 - 40, "Display");
+    let mut disp = TextDisplay::new(
+        WIDGET_PADDING,
+        WIDGET_PADDING,
+        WIDGET_WIDTH - 40,
+        WIDGET_HEIGHT - 40,
+        "Display",
+    );
     disp.set_buffer(text_buffer);
     win.resizable(&disp);
     win.show();
